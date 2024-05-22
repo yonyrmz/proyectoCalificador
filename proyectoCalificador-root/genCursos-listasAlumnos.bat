5dc59d441a5b05ecb2fa25494664c9b5ee623e64
yonyrmz <tecnopcfull@gmail.com>
2024-05-22 01:10:37 -0300

    modificación al archivo genCursos, ahora crea el directorio + un archivo .md para las listas de alumnos

---
+++ proyectoCalificador-root/genCursos-listasAlumnos.bat
@@ -0,0 +1,77 @@
+@echo off
+chcp 65001 > nul
+md "Cursos"
+cd "Cursos"
+    md "1º"
+    cd "1º"
+        md "1º - A"
+        cd "1º - A"
+            1º - A/listaAlumnos.md
+            echo. >listaAlumnos.md
+    cd ..
+        md "1º - B"
+        cd "1º - B"
+            1º - B/listaAlumnos.md
+            echo. >listaAlumnos.md
+        cd ..
+        md "1º - C"
+        cd "1º - C"
+            1º - C/listaAlumnos.md
+            echo. >listaAlumnos.md
+    cd ..
+cd ..
+    md "2º"
+    cd "2º"
+        md "2º - A"
+        cd "2º - A"
+            2º - A/listaAlumnos.md
+            echo. >listaAlumnos.md
+    cd ..
+        md "2º - B"
+        cd "2º - B"
+            2º - B/listaAlumnos.md
+            echo. >listaAlumnos.md
+        cd ..
+        md "2º - C"
+        cd "2º - C"
+            2º - C/listaAlumnos.md
+            echo. >listaAlumnos.md
+    cd ..
+cd ..
+    md "3º"
+    cd "3º"
+        md "3º - A"
+        cd "3º - A"
+                3º - A/listaAlumnos.md
+                echo. >listaAlumnos.md
+    cd ..
+        md "3º - B"
+        cd "3º - B"
+            3º - B/listaAlumnos.md
+                echo. >listaAlumnos.md
+    cd ..
+cd ..
+        md "4º"
+        cd "4º"
+        md "4º - A"
+        cd "4º - A"
+            4º - A/listaAlumnos.md
+                echo. >listaAlumnos.md
+    cd ..
+cd ..    
+        md "5º"
+        cd "5º"
+        md "5º - A"
+        cd "5º - A"
+            5º - A/listaAlumnos.md
+                echo. >listaAlumnos.md
+    cd ..
+cd ..
+        md "6º"
+        cd "6º"
+        md "6º - A"
+        cd "6º - A"
+            6º - A/listaAlumnos.md
+                echo. >listaAlumnos.md
+    cd ..
+cd ..
